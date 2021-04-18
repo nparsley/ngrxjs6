8c78d7514f01ffd42d75aac3042dd7045f98e9ee
nparsley <nickparsley5@gmail.com>
2021-04-18 16:26:50 -0700

    aboutcomponent-courses observable

--- rxjsoperators1/src/app/about/about.component.ts
+++ rxjsoperators1/src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { fromEvent, interval, timer } from 'rxjs';
+import { response } from 'express';
+import { fromEvent, interval, Observable, timer } from 'rxjs';
 import { count } from 'rxjs/operators';
 
 @Component({
@@ -13,57 +14,28 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-/*     --STREAM OF VALUES, AN INTERVAL OVER TIME
-
-    document.addEventListener('click', evt => {
-      console.log(evt);
-
-      setTimeout(() => {
-
-        console.log("finished...");
-
-        let counter = 0;
-
-        setInterval(() => {
-
-          console.log(counter);
-          counter++;
-
-        }, 1000);
-
-      }, 3000);
-
-    }); */
-
-/*     --OBSERVABLE EX, observable is a blueprint for a stream
-
-    const interval$ = timer(3000, 1000);
-
-    interval$.subscribe(val => console.log("stream 1 => " + val));
-
-    interval$.subscribe(val => console.log("stream 2 => " + val));
-
-    const click$ = fromEvent(document, 'click');
-
-    click$.subscribe(evt => console.log(evt)); */
 
 
-    const interval$ = timer(3000, 1000);
+    const http$ = new Observable(observer => {
+      fetch('/api/courses')
+      .then(response => {
+        return response.json();
+      })
+      .then(body => {
+        observer.next(body);
+        observer.complete();
+      })
+      .catch(err => {
+        observer.error(err);
+      })
 
-    const sub = interval$.subscribe(val => console.log("stream 1 => " + val));
+    });
 
-    setTimeout(() => sub.unsubscribe(), 5000);
-
-    const click$ = fromEvent(document, 'click');
-
-    click$.subscribe(
-      evt => console.log(evt),
-      err => console.log(err),
-      () => console.log("completed")
-
+    http$.subscribe(
+      courses => console.log(courses),
+      () => {}, // noop can be used here in place
+      () => console.log('completed')
     );
-
-
 
   }
 
